61cf1284583f4e65a05dc798c22cfeec3eab936b
Sanimesh96 <sanimesh96@gmail.com>
2023-03-10 17:45:09 +0530

    Templates Made

--- detection/urls.py
+++ detection/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    
+    path('', homepage, name="home"),
+    path('uploadImage/', uploadImage, name = 'uploadImage'),
+    path('dashboard/', dashboard, name = 'dashboard'),
+    path('viewImages/', viewImages, name = 'viewImages'),
+
 ]
--- detection/views.py
+++ detection/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def homepage(request):
+    if request.user.is_authenticated:
+
+        return render(request , 'pages/homepage.html')
+    
+
+def dashboard(request):
+    if request.user.is_authenticated:
+
+        return render(request , 'pages/dashboard.html')
+    
+
+def uploadImage(request):
+    if request.user.is_authenticated:
+
+        return render(request , 'pages/uploadImage.html')
+    
+
+def viewImages(request):
+    if request.user.is_authenticated:
+
+        return render(request , 'pages/viewImages.html')
+    
